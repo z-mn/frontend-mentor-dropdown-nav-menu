d0594e2a61d7846f26be552e97af745383826a43
Zayne <155245405+z-mn@users.noreply.github.com>
2024-09-25 14:54:54 +0100

    fix mobile horizontal overflow

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 const menuToggle = document.getElementById('hamburger-container');
 const hamburgerMenu = document.getElementById('hamburger');
 const sideMenu = document.getElementById('sidebar');
+const navbar = document.getElementById('navigation');
 
 menuToggle.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('navbar__hamburger-menu--active');
     sideMenu.classList.toggle('hamburger-menu__sidebar--hidden');
+    navbar.classList.toggle('overflow-toggle');
 })
